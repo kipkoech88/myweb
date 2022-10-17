facc86b0dd1798e8ba5cf623229db368090e63f1
kipkoech88 <96489816+kipkoech88@users.noreply.github.com>
2022-10-17 10:24:40 +0300

    updated features

--- myweb.js
+++ myweb.js
@@ -1,5 +1,15 @@
-var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
-dataSpyList.forEach(function (dataSpyEl) {
-  bootstrap.ScrollSpy.getInstance(dataSpyEl)
-    .refresh()
-})
+    var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+    dataSpyList.forEach(function (dataSpyEl) {
+    bootstrap.ScrollSpy.getInstance(dataSpyEl)
+        .refresh()
+    }) 
+
+    var scrollSpyContentEl = document.getElementById('content')
+    var scrollSpy = bootstrap.ScrollSpy.getInstance(scrollSpyContentEl) // Returns a Bootstrap scrollspy instance 
+
+    var firstScrollSpyEl = document.querySelector('[data-bs-spy="scroll"]')
+firstScrollSpyEl.addEventListener('activate.bs.scrollspy', function () {
+  // do something...
+}) 
+
+    
