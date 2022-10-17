342bac87c27951121a3f9e4d259a18a58ccf1175
kipkoech88 <96489816+kipkoech88@users.noreply.github.com>
2022-10-17 09:13:43 +0300

    added javascript

---
+++ myweb.js
@@ -0,0 +1,5 @@
+var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+dataSpyList.forEach(function (dataSpyEl) {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl)
+    .refresh()
+})
